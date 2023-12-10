a9800af447b07d52386221b09e63960095c93836
gangbeomsu <kbs21c1987@gmail.com>
2023-12-10 18:39:45 +0900

    이미지 컴포넌트 분리작업

--- corp/src/app/page.tsx
+++ corp/src/app/page.tsx
@@ -1,11 +1,14 @@
-import Image from 'next/image';
-import homeImg from '/public/home.jpg';
+import Hero from '@/components/hero';
+import homeImage from '/public/home.jpg';
 
 export default function Home() {
   return (
-    <div className="absolute -z-10 inset-0">
-      Home
-      <Image src={homeImg} alt="home" fill style={{ objectFit: 'cover' }} />
+    <div className="">
+      <Hero
+        imgData={homeImage}
+        imgAlt="Home"
+        title="Professional Cloud Hosting"
+      />
     </div>
   );
 }
