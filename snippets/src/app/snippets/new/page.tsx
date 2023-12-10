ae9460e7fed9a3b0c42972025a96ec4c1627b7b4
gangbeomsu <kbs21c1987@gmail.com>
2023-12-10 20:39:04 +0900

    데이터 입력 form 컴포넌트 만들기

---
+++ snippets/src/app/snippets/new/page.tsx
@@ -0,0 +1,30 @@
+export default function SnippetCreatePage() {
+  return (
+    <form action="">
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label htmlFor="title" className="w-12">
+            Title
+          </label>
+          <input
+            name="title"
+            id="title"
+            className="border rounded p-2 w-full"
+          />
+        </div>
+        <div className="flex gap-4">
+          <label htmlFor="code" className="w-12">
+            Code
+          </label>
+          <textarea
+            name="code"
+            id="code"
+            className="border rounded p-2 w-full"
+          />
+        </div>
+        <button className="border rounded p-2 bg-blue-200">Create</button>
+      </div>
+    </form>
+  );
+}
