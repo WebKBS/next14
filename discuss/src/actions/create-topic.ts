5b421a7d4826d9216bc3e9f9462c424351dde451
gangbeomsu <kbs21c1987@gmail.com>
2023-12-14 21:14:47 +0900

    create topic 폼 추가

--- discuss/src/actions/create-topic.ts
+++ discuss/src/actions/create-topic.ts
@@ -1,4 +1,9 @@
 'use server';
-export async function createTopic() {
+export async function createTopic(formData: FormData) {
   // 재검증 필요
+
+  const name = formData.get('name');
+  const description = formData.get('description');
+
+  console.log(name, description);
 }
