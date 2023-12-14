8263541cef246d591c546781301ee179ed2eae59
gangbeomsu <kbs21c1987@gmail.com>
2023-12-14 21:25:30 +0900

    zod 를 사용한 유효성 검사 만들기

--- discuss/src/actions/create-topic.ts
+++ discuss/src/actions/create-topic.ts
@@ -1,9 +1,26 @@
 'use server';
+
+import { z } from 'zod';
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: '영어 소문자와 하이픈(-)만 사용할 수 있습니다.',
+    }),
+  description: z.string().min(10),
+});
+
 export async function createTopic(formData: FormData) {
   // 재검증 필요
 
-  const name = formData.get('name');
-  const description = formData.get('description');
+  const result = createTopicSchema.safeParse({
+    name: formData.get('name'),
+    description: formData.get('description'),
+  });
 
-  console.log(name, description);
+  if (!result.success) {
+    console.log(result.error.flatten().fieldErrors);
+  }
 }
