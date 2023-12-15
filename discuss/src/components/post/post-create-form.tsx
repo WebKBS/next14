dcb9907342a4e6071b8d95df322f4e8e0847b292
gangbeomsu <kbs21c1987@gmail.com>
2023-12-15 17:06:41 +0900

    post create 컴포넌트 만들기

--- discuss/src/components/post/post-create-form.tsx
+++ discuss/src/components/post/post-create-form.tsx
@@ -1,3 +1,42 @@
+'use client';
+
+import * as actions from '@/actions';
+import {
+  Button,
+  Input,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@nextui-org/react';
+import FormButton from '../common/form-button';
+
 export default function PostCreateForm() {
-  return <div>post-create-form</div>;
+  const action = actions;
+  return (
+    <Popover placement="left">
+      <PopoverTrigger>
+        <Button color="primary">Create a Post</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <form action="">
+          <div className="flex flex-col gap-4 p-4 w-80">
+            <h3 className="text-lg">Create a Post</h3>
+            <Input
+              name="title"
+              label="Title"
+              labelPlacement="outside"
+              placeholder="Title"
+            />
+            <Input
+              name="content"
+              label="Content"
+              labelPlacement="outside"
+              placeholder="Content"
+            />
+            <FormButton>Create Post</FormButton>
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
 }
