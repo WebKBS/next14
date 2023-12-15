97b193e2cc8ed0b380029cf8e19c2e7cdae630d7
gangbeomsu <kbs21c1987@gmail.com>
2023-12-15 19:37:56 +0900

    comment 구성요소 셋팅 및 파일 이름변경

--- discuss/src/app/topics/[slug]/posts/[postId]/page.tsx
+++ discuss/src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,3 +1,24 @@
-export default function PostShowPage() {
-  return <div>Post Show page</div>;
+import paths from '@/paths';
+import Link from 'next/link';
+
+interface PostShowPageProps {
+  params: {
+    slug: string;
+    postId: string;
+  };
+}
+
+export default async function PostShowPage({ params }: PostShowPageProps) {
+  const { slug, postId } = params;
+
+  return (
+    <div className="space-y-3">
+      <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
+        {'< '}Back to {slug}
+      </Link>
+      {/* <PostShow /> */}
+      {/* <CommentCreateForm postId={postId} startOpen /> */}
+      {/* <CommentList comments={comments} /> */}
+    </div>
+  );
 }
