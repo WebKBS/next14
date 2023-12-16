57ee4edfbdb1bf5b4e2ee37fbbb2c228f74bca48
gangbeomsu <kbs21c1987@gmail.com>
2023-12-16 19:52:46 +0900

    postId 로 comment Fetching 변경하고 콘솔 확인하기

--- discuss/src/db/queries/comments.ts
+++ discuss/src/db/queries/comments.ts
@@ -8,6 +8,8 @@ export type CommentWithAuthor = Comment & {
 export function fetchCommentsByPostId(
   postId: string
 ): Promise<CommentWithAuthor[]> {
+  console.log('코멘트를 불러옵니다.');
+
   return db.comment.findMany({
     where: { postId },
     include: { user: { select: { name: true, image: true } } },
