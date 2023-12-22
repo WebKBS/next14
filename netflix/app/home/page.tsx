603afaa75e843ca6c8ea9cd0d538ddc046beec65
gangbeomsu <kbs21c1987@gmail.com>
2023-12-22 20:28:16 +0900

    데이터 불러와서 화면에 추가하기

--- netflix/app/home/page.tsx
+++ netflix/app/home/page.tsx
@@ -1,3 +1,10 @@
+import MovieVideo from '../components/MovieVideo';
+
 export default function HomePage() {
-  return <h1>Hello</h1>;
+  return (
+    <div className="p-5 lg:p-0">
+      <MovieVideo />
+      <h1 className="text-3xl font-bold">Recently Added</h1>
+    </div>
+  );
 }
