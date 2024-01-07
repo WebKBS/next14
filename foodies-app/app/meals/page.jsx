6515feba97baec6134c7d06759c4c600a46f81c9
gangbeomsu <kbs21c1987@gmail.com>
2024-01-07 17:57:56 +0900

    meal 페이지 만들기

--- foodies-app/app/meals/page.jsx
+++ foodies-app/app/meals/page.jsx
@@ -1,3 +1,23 @@
+import MealsGrid from '@/components/meals/meals-grid';
+import Link from 'next/link';
+import classes from './page.module.css';
+
 export default function MealsPage() {
-  return <h1>Meals Page</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}>By you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself.</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
