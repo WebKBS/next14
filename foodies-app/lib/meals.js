36b3250134ba28504df111f7f6f90c5d4d97bdf2
gangbeomsu <kbs21c1987@gmail.com>
2024-01-08 18:52:48 +0900

    보안: slugify와 xss 설치후 사용하기

--- foodies-app/lib/meals.js
+++ foodies-app/lib/meals.js
@@ -1,4 +1,6 @@
 import sql from 'better-sqlite3';
+import slugify from 'slugify';
+import xss from 'xss';
 
 const db = sql('meals.db');
 
@@ -12,4 +14,10 @@ export async function getMeals() {
 export async function getMeal(slug) {
   // await new Promise((resolve) => setTimeout(resolve, 3000));
   return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+}
+
+// slugify, xss 설치
+export async function saveMeal(meal) {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
 }
