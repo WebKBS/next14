0ab630f26c08c5535a8bcd79b316cc9018a4f523
gangbeomsu <kbs21c1987@gmail.com>
2024-01-08 18:54:13 +0900

    보안: 주석추가

--- foodies-app/lib/meals.js
+++ foodies-app/lib/meals.js
@@ -18,6 +18,9 @@ export async function getMeal(slug) {
 
 // slugify, xss 설치
 export async function saveMeal(meal) {
+  // slugify는 문자열을 URL에 사용하기 적합한 형태(slug형태)로 변환해주는 라이브러리
+  // xss는 사용자가 입력한 HTML을 브라우저에서 실행되지 않도록 방지해주는 라이브러리
+
   meal.slug = slugify(meal.title, { lower: true });
   meal.instructions = xss(meal.instructions);
 }
