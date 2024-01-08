01f8b725f95277ae10be38fa0f64f832dd48fc7d
gangbeomsu <kbs21c1987@gmail.com>
2024-01-08 19:57:52 +0900

    useFormState 추가

--- foodies-app/lib/meals.js
+++ foodies-app/lib/meals.js
@@ -19,6 +19,8 @@ export async function getMeal(slug) {
 
 // slugify, xss 설치
 export async function saveMeal(meal) {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
   // slugify는 문자열을 URL에 사용하기 적합한 형태(slug형태)로 변환해주는 라이브러리
   // xss는 사용자가 입력한 HTML을 브라우저에서 실행되지 않도록 방지해주는 라이브러리
 
