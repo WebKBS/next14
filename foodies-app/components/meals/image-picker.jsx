6cbcad022a90bab3d22f246a47eb4569890b836b
gangbeomsu <kbs21c1987@gmail.com>
2024-01-08 18:18:25 +0900

    image-picker 로직 추가

--- foodies-app/components/meals/image-picker.jsx
+++ foodies-app/components/meals/image-picker.jsx
@@ -1,19 +1,41 @@
 'use client';
 
-import { useRef } from 'react';
+import Image from 'next/image';
+import { useRef, useState } from 'react';
 import classes from './image-picker.module.css';
 
 export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
   const imageInput = useRef();
 
   function handlePickClick() {
     imageInput.current.click();
   }
 
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No Image</p>}
+          {pickedImage && <Image src={pickedImage} fill alt="selected image" />}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -21,6 +43,7 @@ export default function ImagePicker({ label, name }) {
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInput}
+          onChange={handleImageChange}
         />
         <button
           className={classes.button}
