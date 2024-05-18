a79a2d3f8c0f7b447df87de80c7c89e2326ced19
gangbeomsu <kbs21c1987@gmail.com>
2024-05-18 11:38:12 +0900

    catch all 라우트로 필터링해서  ui 보여주기

--- routing/app/archive/@archive/[[...filter]]/page.tsx
+++ routing/app/archive/@archive/[[...filter]]/page.tsx
@@ -1,4 +1,10 @@
-import { getAvailableNewsYears } from '@/lib/news';
+import NewsList from '@/components/news-list';
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from '@/lib/news';
 import Link from 'next/link';
 
 const FilteredNewsPage = ({
@@ -8,20 +14,50 @@ const FilteredNewsPage = ({
 }) => {
   console.log(filter);
 
-  const links = getAvailableNewsYears();
+  const selectedYear = filter?.[0];
+
+  const selectedMonth = filter?.[1];
+
+  let news: any;
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(+selectedYear);
+    links = getAvailableNewsMonths(+selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(+selectedYear, +selectedMonth);
+    links = [];
+  }
+
+  let newsContent = <p>뉴스가 없습니다.</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
 
   return (
-    <header id="archive-header">
-      <nav>
-        <ul>
-          {links.map((link) => (
-            <li key={link}>
-              <Link href={`/archive/${link}`}>{link}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
   );
 };
 
