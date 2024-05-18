ccdfe85781e30ba91d92a32f30a078fef0b8c441
gangbeomsu <kbs21c1987@gmail.com>
2024-05-18 11:56:19 +0900

    클라이언트 컴포넌트 분리

---
+++ routing/components/nav-link.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const NavLink = ({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) => {
+  const pathname = usePathname();
+
+  return (
+    <Link href={href} className={pathname.startsWith(href) ? 'active' : ''}>
+      {children}
+    </Link>
+  );
+};
+
+export default NavLink;
