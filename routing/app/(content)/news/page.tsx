2c9e807478e4db4725188dcb74019b74801cb32a
gangbeomsu <kbs21c1987@gmail.com>
2024-05-26 19:15:54 +0900

    server fetch

--- routing/app/(content)/news/page.tsx
+++ routing/app/(content)/news/page.tsx
@@ -1,49 +1,17 @@
-'use client';
-
 import NewsList from '@/components/news-list';
-import { useEffect, useState } from 'react';
 
-const NewsPage = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<null | string>(null);
-  const [news, setNews] = useState([]);
+const NewsPage = async () => {
+  const response = await fetch('http://localhost:8080/news');
 
-  useEffect(() => {
-    async function fetchNews() {
-      setIsLoading(true);
-      const response = await fetch('http://localhost:8080/news');
-      if (!response.ok) {
-        setError('News 데이터를 불러오는데 실패했습니다.');
-        setIsLoading(false);
-        throw new Error('News 데이터를 불러오는데 실패했습니다.');
-      }
-
-      const news = await response.json();
-      setNews(news);
-      setIsLoading(false);
-    }
-
-    fetchNews();
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
+  if (!response.ok) {
+    throw new Error('Failed to fetch news');
   }
-
-  if (error) {
-    return <p>{error}</p>;
-  }
-
-  let newsContent;
-
-  if (news) {
-    newsContent = <NewsList news={news} />;
-  }
+  const news = await response.json();
 
   return (
     <>
       <h1>News NewsPage</h1>
-      {newsContent}
+      <NewsList news={news} />
     </>
   );
 };
