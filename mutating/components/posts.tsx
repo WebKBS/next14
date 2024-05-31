1cd3dc6a4fbd7365b5b8ec400bf354673787bc27
gangbeomsu <kbs21c1987@gmail.com>
2024-05-31 21:24:00 +0900

    낙관적 업데이트 추가

--- mutating/components/posts.tsx
+++ mutating/components/posts.tsx
@@ -1,8 +1,10 @@
+'use client';
 import { togglePostLikeStatus } from '@/actions/posts';
 import { formatDate } from '@/lib/format';
+import { useOptimistic } from 'react';
 import LikeButton from './like-icon';
 
-function Post({ post }: { post: any }) {
+function Post({ post, action }: { post: any; action: any }) {
   return (
     <article className="post">
       <div className="post-image">
@@ -21,7 +23,7 @@ function Post({ post }: { post: any }) {
           </div>
           <div>
             <form
-              action={togglePostLikeStatus.bind(null, post.id)}
+              action={action.bind(null, post.id)}
               className={post.isLiked ? 'liked' : ''}
             >
               <LikeButton />
@@ -35,15 +37,41 @@ function Post({ post }: { post: any }) {
 }
 
 export default function Posts({ posts }: { posts: any[] }) {
+  const [optimisticPosts, updateOptimisticPosts] = useOptimistic(
+    posts,
+    (prevPosts: any, updatedPostId) => {
+      const updatedPostIndex = prevPosts.findIndex(
+        (post: any) => post.id === updatedPostId
+      );
+
+      if (updatedPostIndex === -1) {
+        return prevPosts;
+      }
+
+      const updatedPost = { ...prevPosts[updatedPostIndex] };
+      updatedPost.likes = updatedPost.likes + (updatedPost.isLiked ? -1 : 1);
+      updatedPost.isLiked = !updatedPost.isLiked;
+      const newPosts = [...prevPosts];
+      newPosts[updatedPostIndex] = updatedPost;
+
+      return newPosts;
+    }
+  ); // 첫번째 인자로 posts를 넣어주면 posts가 변경될 때마다 이 함수가 호출된다.
+
   if (!posts || posts.length === 0) {
     return <p>There are no posts yet. Maybe start sharing some?</p>;
+  }
+
+  async function updatePost(postId: number) {
+    updateOptimisticPosts(postId);
+    await togglePostLikeStatus(postId);
   }
 
   return (
     <ul className="posts">
       {posts.map((post) => (
         <li key={post.id}>
-          <Post post={post} />
+          <Post post={post} action={updatePost} />
         </li>
       ))}
     </ul>
